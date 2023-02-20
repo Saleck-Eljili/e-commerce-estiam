2eec4e53192a7f1be0e5b21cace76be0f5278bd3
Saleck-Eljili <salek.eljili@gmail.com>
2023-02-20 14:56:53 +0100

    feat: fack api

---
+++ screens/Cart.js
@@ -0,0 +1,241 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, Feather, FontAwesome5 } from "@expo/vector-icons";
+import Colors from "../utilities/Colors";
+import cartItems from "./CartItems";
+
+export default function Cart({ navigation }) {
+  const [brand, setBrand] = useState("Nike"); // state to handle which brand's shoes are diplayed
+  let totalPrice = 0;
+  console.log(cartItems);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Homepage top-nav */}
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => {
+            navigation.navigate("Home");
+          }}
+        >
+          <Ionicons name="chevron-back" size={24} color={Colors.titleColor} />
+        </TouchableOpacity>
+
+        <View style={{ flex: 1 }}>
+          <Text
+            style={{
+              color: Colors.titleColor,
+              fontWeight: "bold",
+              alignSelf: "center",
+            }}
+          >
+            Cart
+          </Text>
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <FontAwesome5
+            name="bookmark"
+            size={22}
+            color={Colors.titleColor}
+            style={{ alignSelf: "flex-end" }}
+          />
+        </View>
+      </View>
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {/* cart items*/}
+        <View style={{ marginTop: 20 }}>
+          {cartItems.map((item, index) => {
+            // update totalPrice
+            totalPrice += parseInt(item.price);
+            return (
+              <View
+                key={index}
+                style={{
+                  flexDirection: "row",
+                  backgroundColor: "#fff",
+                  padding: 15,
+                  borderRadius: 15,
+                  marginVertical: 7,
+                }}
+              >
+                <View
+                  style={{
+                    backgroundColor: Colors.background,
+                    padding: 15,
+                    borderRadius: 15,
+                    marginRight: 10,
+                  }}
+                >
+                  <Image
+                    source={item.images[0]}
+                    style={{ width: 80, height: 50 }}
+                  />
+                </View>
+
+                <View style={{ flex: 1 }}>
+                  <Text
+                    style={{
+                      fontWeight: "700",
+                      fontSize: 16,
+                      color: Colors.titleColor,
+                      marginBottom: 2,
+                    }}
+                  >
+                    {item.name}
+                  </Text>
+                  <Text style={{ color: Colors.textPrimary, marginBottom: 5 }}>
+                    Men's Shoe
+                  </Text>
+
+                  <View style={{ flexDirection: "row", alignItems: "center" }}>
+                    <View
+                      style={{
+                        flexDirection: "row",
+                        flex: 3,
+                        alignItems: "baseline",
+                      }}
+                    >
+                      <Text
+                        style={{
+                          fontWeight: "bold",
+                          fontSize: 16,
+                          color: Colors.titleColor,
+                          marginBottom: 2,
+                        }}
+                      >
+                        €{item.price}
+                      </Text>
+                      <Text>.00</Text>
+                    </View>
+
+                    <View
+                      style={{
+                        alignSelf: "flex-end",
+                        flexDirection: "row",
+                        alignItems: "center",
+                        padding: 10,
+                        backgroundColor: Colors.background,
+                        marginLeft: 15,
+                      }}
+                    >
+                      <View
+                        style={{
+                          backgroundColor: "#fff",
+                          paddingVertical: 0,
+                          paddingHorizontal: 9,
+                          marginHorizontal: 5,
+                          borderRadius: 6,
+                        }}
+                      >
+                        <Text
+                          style={{ fontSize: 17, color: Colors.titleColor }}
+                        >
+                          -
+                        </Text>
+                      </View>
+                      <Text style={{ fontWeight: "bold", marginHorizontal: 2 }}>
+                        1
+                      </Text>
+                      <View
+                        style={{
+                          backgroundColor: Colors.primary,
+                          paddingVertical: 0,
+                          paddingHorizontal: 9,
+                          marginHorizontal: 5,
+                          borderRadius: 6,
+                        }}
+                      >
+                        <Text style={{ fontSize: 17, color: "#fff" }}>+</Text>
+                      </View>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+
+        {/* Total amount */}
+        <View style={{ marginTop: 50 }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text
+              style={{
+                fontSize: 17,
+                fontWeight: "bold",
+                color: Colors.titleColor,
+                flex: 1,
+              }}
+            >
+              Total
+            </Text>
+            <View>
+              <Text
+                style={{
+                  fontSize: 17,
+                  fontWeight: "bold",
+                  color: Colors.titleColor,
+                  flex: 1,
+                }}
+              >
+                {totalPrice}
+                <Text
+                  style={{
+                    fontSize: 15,
+                    fontWeight: "bold",
+                    color: Colors.titleColor,
+                    flex: 1,
+                  }}
+                >
+                  .00
+                </Text>
+              </Text>
+            </View>
+          </View>
+
+          {/* Checkout button */}
+          <TouchableOpacity
+            style={{
+              backgroundColor: Colors.primary,
+              alignItems: "center",
+              paddingVertical: 20,
+              marginHorizontal: 0,
+              marginVertical: 20,
+              borderRadius: 15,
+            }}
+            onPress={() => navigation.navigate("Checkout")}
+          >
+            <Text style={{ color: "#fff" }}>Checkout</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 22,
+    marginTop: 10,
+    backgroundColor: Colors.background,
+  },
+});
---
+++ screens/Home.js
@@ -0,0 +1,273 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  Image,
+} from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, Feather, FontAwesome5 } from "@expo/vector-icons";
+import Colors from "../utilities/Colors";
+import Products from "../utilities/database";
+
+export default function Home({ navigation }) {
+  const [brand, setBrand] = useState("Nike"); // state to handle which brand's shoes are diplayed
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {/* Homepage top-nav */}
+      <View
+        style={{
+          flexDirection: "row",
+          marginTop: 10,
+        }}
+      >
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => navigation.navigate("Profile")}
+        >
+          <Ionicons name="options" size={24} color={Colors.titleColor} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => navigation.navigate("Cart")}
+        >
+          <Ionicons
+            name="cart"
+            size={24}
+            color={Colors.titleColor}
+            style={{ alignSelf: "flex-end" }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      <View style={{ marginVertical: 20 }}>
+        <Text
+          style={{ fontSize: 21, fontWeight: "bold", color: Colors.titleColor }}
+        >
+          Perfect Shoes
+        </Text>
+        <Text
+          style={{
+            fontSize: 15.5,
+            fontStyle: "italic",
+            color: Colors.titleColor,
+          }}
+        >
+          For perfect style
+        </Text>
+      </View>
+
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        {/* search bar */}
+        <View
+          style={{
+            flex: 4,
+            flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: "#fff",
+            padding: 10,
+            marginRight: 30,
+            borderRadius: 12,
+          }}
+        >
+          <Feather
+            name="search"
+            size={24}
+            color="black"
+            style={{ paddingHorizontal: 10 }}
+          />
+          <TextInput placeholder="search" style={{ marginRight: 40 }} />
+        </View>
+
+        {/* filter  icon */}
+        <View
+          style={{
+            // flex: 1,
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            backgroundColor: Colors.primary,
+            padding: 10,
+            borderRadius: 12,
+          }}
+        >
+          <Ionicons name="ios-options-sharp" size={24} color="white" />
+        </View>
+      </View>
+
+      {/* Product listings based on categories */}
+      <ProductList
+        values={["Nike", "Puma", "Adidas", "New Balance"]}
+        selectedBrand={brand}
+        setSelectedBrand={setBrand}
+        navigation={navigation}
+        products={Products}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 22,
+    marginTop: 10,
+    backgroundColor: Colors.background,
+  },
+  product: {
+    backgroundColor: "#fff",
+    paddingHorizontal: 7,
+    paddingVertical: 18,
+    borderRadius: 15,
+    marginBottom: 15,
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 3,
+    //   height: 2,
+    // },
+    // shadowOpacity: 1.25,
+    // shadowRadius: 0.84,
+    elevation: 1,
+  },
+
+  brandButtons: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    backgroundColor: "#fff",
+    borderRadius: 10,
+  },
+  selectedBrandButton: {
+    backgroundColor: Colors.primary,
+  },
+  brandLabel: {
+    color: "black",
+  },
+  selectedBrandLabel: {
+    color: "#fff",
+  },
+});
+
+const ProductList = ({
+  values,
+  products,
+  selectedBrand,
+  setSelectedBrand,
+  navigation,
+}) => (
+  <View>
+    {/* shoe brands */}
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginTop: 15,
+        marginBottom: 5,
+      }}
+    >
+      {values.map((value) => {
+        return (
+          <TouchableOpacity
+            key={value}
+            style={[
+              styles.brandButtons,
+              selectedBrand == value && styles.selectedBrandButton,
+            ]}
+            onPress={() => setSelectedBrand(value)}
+          >
+            <Text
+              style={[
+                styles.brandLabel,
+                selectedBrand == value && styles.selectedBrandLabel,
+              ]}
+            >
+              {value}
+            </Text>
+          </TouchableOpacity>
+        );
+      })}
+    </View>
+
+    <ScrollView
+      showsVerticalScrollIndicator={false}
+      style={{ marginBottom: 20 }}
+    >
+      <View
+        style={{
+          flex: 1,
+          flexDirection: "row",
+          marginTop: 12,
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {products.map((product, index) => {
+          if (product.type == selectedBrand) {
+            return (
+              <TouchableOpacity
+                key={product.name}
+                style={styles.product}
+                onPress={() => {
+                  navigation.navigate("Product", { index: index });
+                }}
+              >
+                <FontAwesome5
+                  name="bookmark"
+                  size={22}
+                  color="black"
+                  style={{
+                    alignSelf: "flex-end",
+                    color: Colors.textSecondary,
+                    marginRight: 15,
+                    marginTop: 8,
+                  }}
+                />
+                <Image
+                  source={product.images[0]}
+                  style={{ width: 160, height: 85, marginBottom: 15 }}
+                />
+                <View style={{ paddingLeft: 10 }}>
+                  <Text
+                    style={{
+                      color: Colors.titleColor,
+                      fontSize: 14,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {product.name}
+                  </Text>
+                  <View
+                    style={{ flexDirection: "row", alignItems: "baseline" }}
+                  >
+                    <Text
+                      style={{
+                        color: Colors.textSecondary,
+                        fontSize: 14,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      €{product.price}
+                    </Text>
+                    <Text
+                      style={{
+                        color: Colors.textSecondary,
+                        fontSize: 12,
+                        fontWeight: "bold",
+                      }}
+                    >
+                      .00
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            );
+          }
+        })}
+      </View>
+    </ScrollView>
+  </View>
+);
